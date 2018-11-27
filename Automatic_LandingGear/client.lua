aec96ac20ffcd5e02d4d222fef477fc9f1a36286
BluemanGaming <39203615+BluemanGaming@users.noreply.github.com>
2018-11-27 13:59:02 +0100

    Update client.lua

--- Automatic_LandingGear/client.lua
+++ Automatic_LandingGear/client.lua
@@ -5,17 +5,19 @@ Citizen.CreateThread(function()
 	while true do
 		local playerped = GetPlayerPed(-1)
 			if IsPedInAnyVehicle(playerped , false) then
-				local veh = GetVehiclePedIsUsing(playerped)
-        if GetEntityHeightAboveGround(GetVehiclePedIsUsing(PlayerPedId()))>10.0 then
-            GetLandingGearState(aircraft, 2)
-            ControlLandingGear(aircraft, 1)
-            TriggerEvent('chatMessage', "Plane", { 255, 26, 71 }, " Landing Gear Deployed")
-        elseif GetEntityHeightAboveGround(GetVehiclePedIsUsing(PlayerPedId()))<10.0 then
-            GetLandingGearState(aircraft, 1)
-            ControlLandingGear(aircraft, 2)
-            TriggerEvent('chatMessage', "Plane", { 255, 26, 71 }, " Landing Gear Retracted")
+                local veh = GetVehiclePedIsUsing(playerped)
+                if GetEntityHeightAboveGround(GetVehiclePedIsUsing(PlayerPedId()))>10.0 then
+                    GetLandingGearState(aircraft, 2)
+                    ControlLandingGear(aircraft, 1)
+                    TriggerEvent('chatMessage', "Plane", { 255, 26, 71 }, " Landing Gear Deployed")
+                elseif GetEntityHeightAboveGround(GetVehiclePedIsUsing(PlayerPedId()))<10.0 then
+                    GetLandingGearState(aircraft, 1)
+                    ControlLandingGear(aircraft, 2)
+                    TriggerEvent('chatMessage', "Plane", { 255, 26, 71 }, " Landing Gear Retracted")
+                end
+                Citizen.Wait(0)
+            end
         end
-        Citizen.Wait(0)
     end
 end)
         
