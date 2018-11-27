932bb5e0d1c2bc7afef868e7dc1f36b132c1ddd6
BluemanGaming <39203615+BluemanGaming@users.noreply.github.com>
2018-11-27 12:47:11 +0100

    Add files via upload

---
+++ Automatic_LandingGear/client.lua
@@ -0,0 +1,33 @@
+local Ped = GetPlayerPedId
+local Vehicle = GetVehiclePedIsUsing
+
+
+GetEntityHeightAboveGround(GetVehiclePedIsUsing(PlayerPedId()))>10.0 then
+    GetLandingGearState(aircraft, 2)
+    ControlLandingGear(aircraft, 1)
+    TriggerEvent('chatMessage', "Plane", { 255, 26, 71 } " Landing Gear Deployed")
+elseif GetEntityHeightAboveGround(GetVehiclePedIsUsing(PlayerPedId()))<10.0 then
+    GetLandingGearState(aircraft, 1)
+    ControlLandingGear(aircraft, 2)
+    TriggerEvent('chatMessage', "Plane", { 255, 26, 71 } " Landing Gear Retracted")
+end
+
+
+
+
+
+
+
+
+
+
+
+-- // 0xCFC8BE9A5E1FE575 0x24F873FB
+-- // ControlLandingGear
+-- void CONTROL_LANDING_GEAR(Vehicle vehicle, int state);
+-- Works for vehicles with a retractable landing gear  
+-- landing gear states:  
+-- 0: Deployed  
+-- 1: Closing  
+-- 2: Opening  
+-- 3: Retracted 
